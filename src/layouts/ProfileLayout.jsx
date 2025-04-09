58342bd4e42b8519e1000d9286e2f379fda36ddf
Fabrice Karenzi <kafabkaren@gmail.com>
2025-04-09 11:26:40 +0200

    Updated profile section and styling

--- src/layouts/ProfileLayout.jsx
+++ src/layouts/ProfileLayout.jsx
@@ -1,36 +1,54 @@
+import { Link } from "react-router-dom";
+// import { resume } from "../assets/docs/fabrice-karenzi-cv.pdf"
 
 const ProfileLayout = () => {
   return (
     <div id="profile-section" className="profile-section">
-        <div className="section-container">
-            <div className="profile-content">
-                <div className="profile-description">
-                    <div className="descr-1">Hi, my name is</div>
-                        <h1 className="descr-2"><span className="first-name">Fabrice</span><span className="lastname">Karenzi</span>,</h1>
-                        <p className="descr-text"> A <span className="profile-dev-typ">.NET Web Developer</span> with passion to build apps and create web experience.</p>
-                    <div className="profile-descr--buttons">
-                        <button className="btn bnt-primary">Get in Touch</button>
-                        <button className="btn bnt-primary">Resume <i className="fa fa-download" aria-hidden="true"></i></button>
-                    </div>
-
-                    <div className="profile-social--networks">
-                    <a href="https://github.com/kafadotnet" target="_blank" rel="noopener noreferrer">
-                        <i className="fa-brands fa-square-github"></i>
-                    </a>
-                    <a href="https://linkedin.com/in/fabrice-karenzi" target="_blank" rel="noopener noreferrer">
-                        <i className="fa-brands fa-linkedin"></i>
-                    </a>
-                    </div>
-
-                </div>
-
-                <div className="profile-img">
-
-                </div>
+      <div className="section-container container">
+        <div className="profile-content row align-items-center">
+          <div className="profile-description col-12 col-md-6">
+            <p className="descr-1">Hi, my name is</p>
+            <h3 className="descr-name">
+              <span className="firstname">Fabrice</span> 
+              <span className="lastname">Karenzi</span>,
+            </h3>
+            <p className="descr-text">
+              A <span className="profile-dev-typ">.NET Web Developer</span> with
+              passion to build apps and create web experience.
+            </p>
+            <div className="profile-descr--buttons">
+              <Link to="/contact-me" className="btn-get-in-touch">Get in Touch</Link>
+              <a href="fabrice-karenzi-cv.pdf" download="fabrice-karenzi-cv.pdf" className="btn-resume">
+                Resume <i className="fa fa-download" aria-hidden="true"></i>
+              </a>
             </div>
+
+            <div className="profile-social--networks">
+              <a className="social-icon"
+                href="https://github.com/kafadotnet"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa-brands fa-square-github"></i>
+              </a>
+              <a className="social-icon"
+                href="https://linkedin.com/in/fabrice-karenzi"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa-brands fa-linkedin"></i>
+              </a>
+            </div>
+          </div>
+
+          <div className="profile-img col-12 col-md-6 text-center">
+            <div className="profile-img-bg">
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default ProfileLayout;
