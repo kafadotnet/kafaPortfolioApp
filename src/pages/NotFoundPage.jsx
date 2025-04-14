af1d49258e12d19647051f59c029bd2fa6a101d9
Fabrice Karenzi <kafabkaren@gmail.com>
2025-04-14 18:14:49 +0200

    Updated PageNotFound

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,14 +1,20 @@
+import Footer from "../components/Footer";
 import NavigationLayout from "../layouts/NavigationLayout";
 
 const NotFoundPage = () => {
   return (
     <>
       <header className="bg-gradient-theme">
-        <NavigationLayout/>
+        <NavigationLayout />
       </header>
-        404 - NotFoundPage
+      <section className="section-container page-not-found">
+        <div className="message">
+          404 - NotFoundPage
+        </div>
+      </section>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
 export default NotFoundPage;
