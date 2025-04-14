0dc4cf4e3c780b414567c35c8b52658d259a1872
Fabrice Karenzi <kafabkaren@gmail.com>
2025-04-14 19:08:23 +0200

    Added Testimonial link

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import './mainmin.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import AboutMe from './pages/AboutMe';
+import Stacks from './pages/Stacks';
+import Portfolio from './pages/Portfolio';
+import TestimonialsPage from './pages/TestimonialsPage';
 import ContactPage from './pages/ContactPage';
 import NotFoundPage from './pages/NotFoundPage';
 
@@ -11,6 +15,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={< HomePage />} />
+          <Route path='/about-me' element={< AboutMe />} />
+          <Route path='/stacks' element={<Stacks />} />
+          <Route path='/portfolio' element={<Portfolio />} />
+          <Route path='/testimonials' element={<TestimonialsPage />} />
           <Route path='/contact-me' element={<ContactPage />} />
           <Route path='*' element={<NotFoundPage />} />
         </Routes>
