60858a2d3ae7ea179cb4734fab894611afd3901d
Fabrice Karenzi <kafabkaren@gmail.com>
2025-04-16 15:29:07 +0200

    Adapted navigation menu to responsiveness

--- src/layouts/NavigationLayout.jsx
+++ src/layouts/NavigationLayout.jsx
@@ -1,22 +1,57 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const NavigationLayout = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
   return (
     <section className="navigation" id="navigation">
-            <nav className="section-container">
-                <NavLink to='/' className='logo' end>kafa<span className="logo-dot">.</span><span className="logo-tech">tech</span></NavLink>
-        
-                <div className="nav-menu--links">
-                    <NavLink to='/' className="nav-menu--link" end>Home</NavLink>
-                    <NavLink to='/about-me' className="nav-menu--link" end>About Me</NavLink>
-                    <NavLink to='/stacks' className="nav-menu--link" end>Stacks</NavLink>
-                    <NavLink to='/portfolio' className="nav-menu--link" end>Portfolio</NavLink>
-                    <NavLink to='/testimonials' className="nav-menu--link" end>Testimonials</NavLink>
-                    <NavLink to='/contact-me' className="nav-menu--link" end>Contact Me</NavLink>
-                </div>
-            </nav>
+      <nav className="section-container">
+        <NavLink to="/" className="logo" onClick={closeMenu} end>
+          kafa<span className="logo-dot">.</span>
+          <span className="logo-tech">tech</span>
+        </NavLink>
+
+        <div
+          className={`hamburger-icon ${menuOpen ? "open" : ""}`}
+          onClick={toggleMenu}
+        >
+          <span />
+          <span />
+          <span />
+        </div>
+
+        <div className={`nav-menu--links ${menuOpen ? "show" : ""}`}>
+          <NavLink to="/" className="nav-menu--link" onClick={closeMenu} end>
+            Home
+          </NavLink>
+          <NavLink to="/about-me" className="nav-menu--link" onClick={closeMenu} end>
+            About Me
+          </NavLink>
+          <NavLink to="/stacks" className="nav-menu--link" onClick={closeMenu} end>
+            Stacks
+          </NavLink>
+          <NavLink to="/portfolio" className="nav-menu--link" onClick={closeMenu} end>
+            Portfolio
+          </NavLink>
+          <NavLink to="/testimonials" className="nav-menu--link" onClick={closeMenu} end>
+            Testimonials
+          </NavLink>
+          <NavLink to="/contact-me" className="nav-menu--link" onClick={closeMenu} end>
+            Contact Me
+          </NavLink>
+        </div>
+      </nav>
     </section>
-  )
-}
+  );
+};
 
 export default NavigationLayout;
