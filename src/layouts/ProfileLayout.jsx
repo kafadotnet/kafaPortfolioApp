7abd0428979b93199e51b93f9beb7de0404f39e9
Fabrice Karenzi <kskillup@outlook.com>
2025-04-29 17:44:27 +0200

    Fixed CV button download functionality

--- src/layouts/ProfileLayout.jsx
+++ src/layouts/ProfileLayout.jsx
@@ -8,7 +8,7 @@ const ProfileLayout = () => {
           <div className="profile-description col-12 col-md-6">
             <p className="descr-1">Hi, my name is</p>
             <h3 className="descr-name">
-              <span className="firstname">Fabrice</span> 
+              <span className="firstname">Fabrice</span>
               <span className="lastname">Karenzi</span>,
             </h3>
             <p className="descr-text">
@@ -16,21 +16,30 @@ const ProfileLayout = () => {
               passion to build apps and create web experience.
             </p>
             <div className="profile-descr--buttons">
-              <Link to="/contact-me" className="btn-get-in-touch">Get in Touch</Link>
-              <a href="fabrice-karenzi-cv.pdf" download="fabrice-karenzi-cv.pdf" className="btn-resume">
+              <Link to="/contact-me" className="btn-get-in-touch">
+                Get in Touch
+              </Link>
+              <a
+                href="/data/fabrice-karenzi-cv.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="btn-resume"
+              >
                 Resume <i className="fa fa-download" aria-hidden="true"></i>
               </a>
             </div>
 
             <div className="profile-social--networks">
-              <a className="social-icon"
+              <a
+                className="social-icon"
                 href="https://github.com/kafadotnet"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 <i className="fa-brands fa-square-github"></i>
               </a>
-              <a className="social-icon"
+              <a
+                className="social-icon"
                 href="https://linkedin.com/in/fabrice-karenzi"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -41,8 +50,7 @@ const ProfileLayout = () => {
           </div>
 
           <div className="profile-img col-12 col-md-6 text-center">
-            <div className="profile-img-bg">
-            </div>
+            <div className="profile-img-bg"></div>
           </div>
         </div>
       </div>
